ac2268d773a1a6003c45c3e48f86339c51787ce9
cj <carl.johan.bohwalli@gmail.com>
2026-05-11 14:45:30 +0200

    refactored the data pipeline for experiments 5-6

--- experiments/v5/lstm_v5_large.py
+++ experiments/v5/lstm_v5_large.py
@@ -1,9 +1,10 @@
 import pandas as pd
-import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 from src.visualisation.plotting import visualise_results
-from src.data.preprocessing import preprocess_data
+from src.data.preprocessing import split_and_normalize
+from src.data.preprocessing import add_measurement_noice
+from src.data.preprocessing import create_sequences_by_cycle
 
 # Constants
 WINDOW_SIZE = 60 
@@ -13,45 +14,21 @@ OUTPUT_TARGETS = 4
 feature_cols = ['load', 't_stator', 't_rotor_1', 't_rotor_2', 't_housing']
 target_cols = ['t_stator', 't_rotor_1', 't_rotor_2', 't_housing']
 
-# Data preprocessing
-# ------------------------------------------------------------------------
+# Data preprocessing pipeline
 
 # 1. Load Data
 df = pd.read_csv('drive_cycle_dataset.csv')
 
+# 2. Split and normalize data
 training_split = 80 # 80% training data 20% validation data
-train_x, train_y, val_x, val_y, scaler_input, scaler_target = preprocess_data(df, training_split, feature_cols, target_cols)
+train_x, train_y, val_x, val_y, scaler_target = split_and_normalize(df, training_split, feature_cols, target_cols)
 
-# Adding noice
-noise_level = 0.60  # 60% of the normalized range (0 to 1)
+# 3. Add measurement noice
+train_y = add_measurement_noice(noise_level=0.6, train_y=train_y)
 
-# Generate Gaussian noise matching the shape of your target data
-train_noise = np.random.normal(0, noise_level, size=train_y.shape)
-
-# Add the noise to the ground truth
-train_y = train_y + train_noise
-
-# 3. Cycle-Aware Sequence Generation
-def create_sequences_by_cycle(df_cycles, x_data, y_data, window_size):
-    x_seq, y_seq = [], []
-    for cycle_id in df_cycles['drive_cycle_number'].unique():
-        # Get indices for this cycle
-        indices = np.where(df_cycles['drive_cycle_number'] == cycle_id)[0]
-        # Get start/end in the transformed array
-        start, end = indices[0], indices[-1] + 1
-        
-        cycle_x = x_data[start:end]
-        cycle_y = y_data[start:end]
-        
-        for i in range(len(cycle_x) - window_size):
-            x_seq.append(cycle_x[i : i + window_size])
-            y_seq.append(cycle_y[i + window_size])
-            
-    return np.array(x_seq), np.array(y_seq)
-
-WINDOW_SIZE = 60
-X_train, y_train = create_sequences_by_cycle(train_df, train_x, train_y, WINDOW_SIZE)
-X_val, y_val = create_sequences_by_cycle(val_df, val_x, val_y, WINDOW_SIZE)
+# 3. Adapt format of data for input and output of the model (input: [window_size, feature_cols] output: [target_cols])
+X_train, y_train = create_sequences_by_cycle(df, train_x, train_y, WINDOW_SIZE)
+X_val, y_val = create_sequences_by_cycle(df, val_x, val_y, WINDOW_SIZE)
 
 # ------------------------------------------------------------------------
 
--- experiments/v5/mlp_v5_small_noice.py
+++ experiments/v5/mlp_v5_small_noice.py
@@ -1,57 +1,34 @@
 import pandas as pd
-import numpy as np
-from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras import layers, models
 from src.visualisation.plotting import visualise_results
-from src.data.preprocessing import preprocess_data
+from src.data.preprocessing import split_and_normalize
+from src.data.preprocessing import add_measurement_noice
+from src.data.preprocessing import create_sequences_by_cycle
+
 
 # Constants
-WINDOW_SIZE = 60 
+WINDOW_SIZE = 1
 INPUT_FEATURES = 5 
 OUTPUT_TARGETS = 4
 
 feature_cols = ['load', 't_stator', 't_rotor_1', 't_rotor_2', 't_housing']
 target_cols = ['t_stator', 't_rotor_1', 't_rotor_2', 't_housing']
 
-# Data preprocessing
-# ------------------------------------------------------------------------
+# Data preprocessing pipeline
 
 # 1. Load Data
 df = pd.read_csv('drive_cycle_dataset.csv')
 
+# 2. Split and normalize data
 training_split = 80 # 80% training data 20% validation data
-train_x, train_y, val_x, val_y, scaler_input, scaler_target = preprocess_data(df, training_split, feature_cols, target_cols)
+train_x, train_y, val_x, val_y, scaler_target = split_and_normalize(df, training_split, feature_cols, target_cols)
 
-# Adding noice
-noise_level = 0.01  # 1% of the normalized range (0 to 1)
+# 3. Add measurement noice
+train_y = add_measurement_noice(noise_level=0.01, train_y=train_y)
 
-# Generate Gaussian noise matching the shape of your target data
-train_noise = np.random.normal(0, noise_level, size=train_y.shape)
-val_noise = np.random.normal(0, noise_level, size=val_y.shape)
-
-# Add the noise to the ground truth
-train_y = train_y + train_noise
-
-# 3. Cycle-Aware Step-Wise Data Generation
-def create_step_pairs_by_cycle(df_cycles, x_data, y_data):
-    x_pairs, y_pairs = [], []
-    
-    for cycle_id in df_cycles['drive_cycle_number'].unique():
-        # Get indices for this specific cycle
-        indices = np.where(df_cycles['drive_cycle_number'] == cycle_id)[0]
-        start, end = indices[0], indices[-1] + 1
-        
-        cycle_x = x_data[start:end]
-        cycle_y = y_data[start:end]
-        
-        for i in range(len(cycle_x) - 1): 
-            x_pairs.append(cycle_x[i])      # Current state Features
-            y_pairs.append(cycle_y[i + 1])  # Next state Targets
-            
-    return np.array(x_pairs), np.array(y_pairs)
-
-X_train, y_train = create_step_pairs_by_cycle(train_df, train_x, train_y)
-X_val, y_val = create_step_pairs_by_cycle(val_df, val_x, val_y)
+# 3. Adapt format of data for input and output of the model (input: [window_size, feature_cols] output: [target_cols])
+X_train, y_train = create_sequences_by_cycle(df, train_x, train_y, WINDOW_SIZE)
+X_val, y_val = create_sequences_by_cycle(df, val_x, val_y, WINDOW_SIZE)
 
 model = models.Sequential([
         layers.Dense(64, activation='relu'),
