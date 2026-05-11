a99841c5c1c1c91fbbaa5df175ff0281a9eabe37
cj <carl.johan.bohwalli@gmail.com>
2026-05-11 14:37:50 +0200

    refactored: added the refactored datapipeline to experiments 2-4

--- experiments/v2/lstm_v2.py
+++ experiments/v2/lstm_v2.py
@@ -2,7 +2,8 @@ import pandas as pd
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 from src.visualisation.plotting import visualise_results
-from src.data.preprocessing import preprocess_data
+from src.data.preprocessing import split_and_normalize
+from src.data.preprocessing import create_sequences_by_cycle
 
 # Constants
 WINDOW_SIZE = 60 
@@ -12,14 +13,18 @@ OUTPUT_TARGETS = 4
 feature_cols = ['load', 't_stator', 't_rotor_1', 't_rotor_2', 't_housing']
 target_cols = ['t_stator', 't_rotor_1', 't_rotor_2', 't_housing']
 
-# Data preprocessing
-# ------------------------------------------------------------------------
+# Data preprocessing pipeline
 
 # 1. Load Data
 df = pd.read_csv('drive_cycle_dataset.csv')
 
+# 2. Split and normalize data
 training_split = 80 # 80% training data 20% validation data
-X_train, y_train, X_val, y_val, scaler_input, scaler_target = preprocess_data(df, training_split, feature_cols, target_cols)
+train_x, train_y, val_x, val_y, scaler_target = split_and_normalize(df, training_split, feature_cols, target_cols)
+
+# 3. Adapt format of data for input and output of the model (input: [window_size, feature_cols] output: [target_cols])
+X_train, y_train = create_sequences_by_cycle(df, train_x, train_y, WINDOW_SIZE)
+X_val, y_val = create_sequences_by_cycle(df, val_x, val_y, WINDOW_SIZE)
 
 # ------------------------------------------------------------------------
 
--- experiments/v4/mlp_v4.py
+++ experiments/v4/mlp_v4.py
@@ -1,24 +1,29 @@
 import pandas as pd
 from src.visualisation.plotting import visualise_results
 from tensorflow.keras import layers, models
-from src.data.preprocessing import preprocess_data
+from src.data.preprocessing import split_and_normalize
+from src.data.preprocessing import create_sequences_by_cycle
 
 # Constants
-WINDOW_SIZE = 60 
+WINDOW_SIZE = 1 
 INPUT_FEATURES = 5 
 OUTPUT_TARGETS = 4
 
 feature_cols = ['load', 't_stator', 't_rotor_1', 't_rotor_2', 't_housing']
 target_cols = ['t_stator', 't_rotor_1', 't_rotor_2', 't_housing']
 
-# Data preprocessing
-# ------------------------------------------------------------------------
+# Data preprocessing pipeline
 
 # 1. Load Data
 df = pd.read_csv('drive_cycle_dataset.csv')
 
+# 2. Split and normalize data
 training_split = 80 # 80% training data 20% validation data
-X_train, y_train, X_val, y_val, scaler_input, scaler_target = preprocess_data(df, training_split, feature_cols, target_cols)
+train_x, train_y, val_x, val_y, scaler_target = split_and_normalize(df, training_split, feature_cols, target_cols)
+
+# 3. Adapt format of data for input and output of the model (input: [window_size, feature_cols] output: [target_cols])
+X_train, y_train = create_sequences_by_cycle(df, train_x, train_y, WINDOW_SIZE)
+X_val, y_val = create_sequences_by_cycle(df, val_x, val_y, WINDOW_SIZE)
 
 model = models.Sequential([
         layers.Dense(64, activation='relu'),
